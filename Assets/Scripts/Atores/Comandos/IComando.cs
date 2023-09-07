f072321c26e4d77f75da72b8a98bf2b01ac4e2f6
Marcus Vinícius Pires <136832870+marvipi@users.noreply.github.com>
2023-09-07 17:04:44 -0300

    Atualiza a documentação da interface IComando

--- Assets/Scripts/Atores/Comandos/IComando.cs
+++ Assets/Scripts/Atores/Comandos/IComando.cs
@@ -3,22 +3,22 @@ using UnityEngine;
 namespace Atores
 {
     /// <summary>
-    /// Representa uma ação reversível que pode ser executada por objetos de jogo.
+    /// Representa uma operação reversível que pode ser executada em objetos de jogo comandáveis.
     /// </summary>
     public interface IComando
     {
         /// <summary>
-        /// Indica quantas vezes este comando foi executado sucessivamente em um objeto de jogo.
+        /// Indica quantas vezes este comando foi executado sucessivamente.
         /// </summary>
         public uint QtdRepeticoes { get; }
 
         /// <summary>
-        /// Executa este comando no objeto de jogo passado como argumento.
+        /// Executa este comando no objeto de jogo comandável passado como argumento.
         /// </summary>
         public void Executar(Comandavel comandavel);
 
         /// <summary>
-        /// Reverte as mudanças causadas pela última execução deste comando no objeto de jogo passado como argumento.
+        /// Reverte as mudanças causadas pela última execução deste comando no objeto de jogo comandável passado como argumento.
         /// </summary>
         public void Reverter(Comandavel comandavel);
     }
